5a51cae7a5319d5b13ae09c503dcdadcefa0bc83
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2022-02-05 23:01:20 -0500

    Added Models and Enums

---
+++ RedmondBlog/Models/Blog.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedmondBlog.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters and no more than {1}", MinimumLength = 2)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} characters and no more than {1}", MinimumLength = 2)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
+        public DateTime Created { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
+        public DateTime? Updated { get; set; }
+
+        [Display(Name = "Blog Image")]
+        public byte[] ImageData { get; set; }
+        [Display(Name = "Image Type")]
+        public string ContentType { get; set; }
+
+        [NotMapped]
+        public IFormFile Image { get; set; }
+
+        //Navigation properties
+        public virtual BlogUser Author { get; set; }
+        public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
+    }
+}
---
+++ RedmondBlog/Models/Tag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedmondBlog.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(25, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters", MinimumLength = 2)]
+        public string Text { get; set; }
+
+        //Navigation properties
+        public virtual Post Post { set; get; }
+        public virtual BlogUser Author { get; set; }
+    }
+}
