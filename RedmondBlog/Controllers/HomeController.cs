3a9eae33e27c997e918cddb5088f30c07fb16bca
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2022-02-08 17:14:45 -0500

    Removed stock lib and css files for template

--- RedmondBlog/Controllers/HomeController.cs
+++ RedmondBlog/Controllers/HomeController.cs
@@ -23,7 +23,12 @@ namespace RedmondBlog.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
         {
             return View();
         }
