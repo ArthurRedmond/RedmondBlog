d4bbbf5de238c198f71e96d783b8a624df35e01f
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2022-02-09 22:03:57 -0500

    Created DataService class and registered it as a service

---
+++ RedmondBlog/Services/DataService.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RedmondBlog.Data;
+using RedmondBlog.Enums;
+using RedmondBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedmondBlog.Services
+{
+    public class DataService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<BlogUser> _userManager;
+
+        public DataService(ApplicationDbContext dbContext, RoleManager<IdentityRole> roleManager, UserManager<BlogUser> userManager)
+        {
+            _dbContext = dbContext;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task ManageDataAsync()
+        {
+            await SeedRolesAsync();
+            await SeedUsersAsync();
+        }
+
+        private async Task SeedRolesAsync()
+        {
+            if (_dbContext.Roles.Any())
+            {
+                return;
+            }
+
+            foreach (var role in Enum.GetNames(typeof(BlogRole)))
+            {
+                await _roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+
+        private async Task SeedUsersAsync()
+        {
+            if (_dbContext.Users.Any())
+            {
+                return;
+            }
+
+            var adminUser = new BlogUser()
+            {
+                Email = "Arthur@ArthurRedmond.com",
+                UserName = "Arthur@ArthurRedmond.com",
+                FirstName = "Arthur",
+                LastName = "Redmond",
+                EmailConfirmed = true
+            };
+
+            await _userManager.CreateAsync(adminUser, "Abc@123!");
+
+            await _userManager.AddToRoleAsync(adminUser, BlogRole.Administrator.ToString());
+
+            var modUser = new BlogUser()
+            {
+                Email = "carshopper26@outlook.com",
+                UserName = "carshopper26@outlook.com",
+                FirstName = "Jon",
+                LastName = "Smith",
+                EmailConfirmed = true
+            };
+
+            await _userManager.CreateAsync(modUser, "Abc@123!");
+
+            await _userManager.AddToRoleAsync(modUser, BlogRole.Moderator.ToString());
+        }
+    }
+}
--- RedmondBlog/Startup.cs
+++ RedmondBlog/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RedmondBlog.Data;
 using RedmondBlog.Models;
+using RedmondBlog.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -47,6 +48,9 @@ namespace RedmondBlog
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            //Register my custom DataService class
+            services.AddScoped<DataService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
