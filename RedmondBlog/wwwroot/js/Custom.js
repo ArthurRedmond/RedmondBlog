5f61ce39e12bf0a45fcdfbc3b322cef7acdc0c9a
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2022-02-24 15:35:12 -0500

    Added Sweet Alert to project

--- RedmondBlog/wwwroot/js/Custom.js
+++ RedmondBlog/wwwroot/js/Custom.js
@@ -4,10 +4,24 @@ function AddTag() {
     //Get a referenc to the TagEntry input element
     var tagEntry = document.getElementById("TagEntry");
 
-    //Create new select option
-    let newOption = new Option(tagEntry.value, tagEntry.value);
-    document.getElementById("TagList").options[index++] = newOption;
-
+    let searchResult = search(tagEntry.value);
+    if (searchResult != null) {
+        //Trigger Sweet Alert for condition in searchResult var
+        //Swal.fire({
+        //    title: 'Error!',
+        //    text: 'Do you want to continue',
+        //    icon: 'error',
+        //    confirmButtonText: 'Cool'
+        //})
+        swalWithDarkButton.fire({
+            html: `<span class='fw-bold'>${searchResult.toUpperCase()}</span>`
+        });
+    }
+    else {
+        //Create new select option
+        let newOption = new Option(tagEntry.value, tagEntry.value);
+        document.getElementById("TagList").options[index++] = newOption;
+    }
     //Clear out the TagEntry textbox
     tagEntry.value = "";
     return true;
@@ -15,11 +29,19 @@ function AddTag() {
 
 function DeleteTag() {
     let tagCount = 1;
+    let tagList = document.getElementById("TagList");
+    if (!tagList) return false;
+
+    if (tagList.selectedIndex == -1) {
+        swalWithDarkButton.fire({
+            html: "<span class='fw-bold'>CHOOSE A TAG BEFORE DELETING</span>"
+        });
+        return true;
+    }
+
     while (tagCount > 0) {
-        let tagList = document.getElementById("TagList");
-        let selectedIndex = tagList.selectedIndex;
-        if (selectedIndex >= 0) {
-            tagList.options[selectedIndex] = null;
+        if (tagList.selectedIndex >= 0) {
+            tagList.options[tagList.selectedIndex] = null;
             --tagCount;
         }
         else tagCount = 0;
@@ -44,3 +66,29 @@ function ReplaceTag(tag, index) {
     let newOption = new Option(tag, tag);
     document.getElementById("TagList").options[index] = newOption;
 }
+
+//The search function will detect either an empty or duplicate Tag on the same post
+function search(str) {
+    if (str == "") {
+        return 'Empty tags are not permitted';
+    }
+
+    var tagsEl = document.getElementById('TagList');
+    if (tagsEl) {
+        let options = tagsEl.options;
+        for (let index = 0; index < options.length; index++) {
+            if (options[index].value == str) {
+                return `The Tag #${str} is a duplicate and not permitted`;
+            }
+        }
+    }
+}
+
+const swalWithDarkButton = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-danger btn-sm w-100 btn-outline-dark'
+    },
+    imageUrl: '/img/oops.jpg',
+    timer: 3000,
+    buttonsStyling: false
+});
