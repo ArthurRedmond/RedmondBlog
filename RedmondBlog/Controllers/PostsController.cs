f6f5571c14abc4dc7f707ccb0e39c1bc29b97f66
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2022-02-24 16:25:03 -0500

    Added error detection of duplicate slugs

--- RedmondBlog/Controllers/PostsController.cs
+++ RedmondBlog/Controllers/PostsController.cs
@@ -101,11 +101,24 @@ namespace RedmondBlog.Controllers
 
                 //Create the slug and determine if it is unique
                 var slug = _slugService.UrlFriendly(post.Title);
+
+                var validationError = false;
+
+                if (string.IsNullOrEmpty(slug))
+                {
+                    validationError = true;
+                    ModelState.AddModelError("", "The Title you provided cannot be used as it results in an empty slug.");
+                }
+
+                //Detect incoming duplicate slugs
                 if (!_slugService.IsUnique(slug))
                 {
-                    //Add a Model state error and return the user back to the create view
+                    validationError = true;
                     ModelState.AddModelError("Title", "The Title you provided cannot be used as it results in a duplicate slug.");
+                }
 
+                if (validationError)
+                {
                     ViewData["TagValues"] = string.Join(",", tagValues);
                     return View(post);
                 }
@@ -170,6 +183,7 @@ namespace RedmondBlog.Controllers
             {
                 try
                 {
+                    //Get a copy of the original post
                     var newPost = await _context.Posts.Include(p => p.Tags).FirstOrDefaultAsync(p => p.Id == post.Id);
 
                     newPost.Updated = DateTime.Now;
@@ -177,6 +191,22 @@ namespace RedmondBlog.Controllers
                     newPost.Abstract = post.Abstract;
                     newPost.Content = post.Content;
                     newPost.ReadyStatus = post.ReadyStatus;
+
+                    var newSlug = _slugService.UrlFriendly(post.Title);
+                    if (newSlug != newPost.Slug)
+                    {
+                        if (_slugService.IsUnique(newSlug))
+                        {
+                            newPost.Title = post.Title;
+                            newPost.Slug = newSlug;
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("Title", "This Title cannot be used as it results in a duplicate slug.");
+                            ViewData["TagValues"] = string.Join(",", post.Tags.Select(t => t.Text));
+                            return View(post);
+                        }
+                    }
 
                     if (newImage is not null)
                     {
